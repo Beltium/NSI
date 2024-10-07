1f75b766c638a81e7f9c029393e216754034a7bc
beltium <mathieuhugo2008@gmail.com>
2024-10-07 10:36:48 +0200

    Séance 2 : - Ajout de fonctionnalité sur l'inventaire comme demandé

--- Trimestre 1/Cours 2/Inventaire.py
+++ Trimestre 1/Cours 2/Inventaire.py
@@ -24,6 +24,34 @@ def afficher_inventaire():
     else:
         print("Votre inventaire est vide.")
 
+def afficher_nb_objets():
+    if inventaire:
+        nb_objet_diff = len(inventaire) # Taille de la liste
+
+        nb_objet = 0
+        for objet in inventaire:
+            nb_objet = nb_objet + int(objet[1]) # Ajout du nombre d'objet dans une variable
+
+        print(f"Votre inventaire contient {nb_objet_diff} objets différents et {nb_objet} au total.")
+    else:
+        print("Votre inventaire est vide.")
+
+def afficher_quantité_objet():
+    if inventaire:
+        nom_obj = input("De quelle objet voulez-vous voir le nombre ? ")
+        trouve = False
+        for objet in inventaire:
+            if nom_obj in objet: # Vérifier si l'objet est présent
+                print(f"Vous avez {objet[1]} {objet[0]}.") # Indiquer son nombre
+                trouve = True
+                break
+
+        if not trouve: # Si il n'est pas trouvé, informer l'utilisateur
+                print("Vous n'avez aucun n'exemplaire de cette objet.")
+
+    else:
+        print("Votre inventaire est vide.")
+
 
 # Menu principal
 while True:
@@ -31,9 +59,11 @@ while True:
     print("1. Ajouter un objet")
     print("2. Retirer un objet")
     print("3. Afficher l'inventaire")
-    print("4. Quitter")
+    print("4. Afficher le nombre d'objets de l'inventaire")
+    print("5. Afficher la quantité d'un objet en particulier")
+    print("6. Quitter")
 
-    choix = input("Que voulez-vous faire ? (1/2/3/4) : ")
+    choix = input("Que voulez-vous faire ? (1/2/3/4/5/6) : ")
 
     if choix == "1":
         objet = input("Entrez le nom de l'objet à ajouter : ")
@@ -48,6 +78,12 @@ while True:
         afficher_inventaire()
 
     elif choix == "4":
+        afficher_nb_objets()
+
+    elif choix == "5":
+        afficher_quantité_objet()
+
+    elif choix == "6":
         print("Merci d'avoir joué !")
         break
 
