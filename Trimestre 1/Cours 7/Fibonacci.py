5b240355d7089bc7ea94dab65b3256dae1c830cf
beltium <mathieuhugo2008@gmail.com>
2024-10-07 10:15:33 +0200

    Séance 7 : - Modification de Fibonacci.py :   - création d'une fonction   - correction d'une faute d'orthographe

--- Trimestre 1/Cours 7/Fibonacci.py
+++ Trimestre 1/Cours 7/Fibonacci.py
@@ -1,16 +1,20 @@
 
 # Ce programme permet de calculer les n premiers termes de la suite de Fibonacci
 
-n = int(input("Jusqu'à quel terme voulez-vous calculez la suite de Fibonacci ? "))
-fibonacci = []
+n = int(input("Jusqu'à quel terme voulez-vous calculer la suite de Fibonacci ? "))
 
-for i in range(n):
-    if i == 0:
-        fibonacci.append(0)
-    elif i == 1:
-        fibonacci.append(1)
-    else:
-        fibonacci.append(fibonacci[-1] + fibonacci[-2])
+def fibonacci(n):
+    fibonacci = []
+
+    for i in range(n):
+        if i == 0:
+            fibonacci.append(0)
+        elif i == 1:
+            fibonacci.append(1)
+        else:
+            fibonacci.append(fibonacci[-1] + fibonacci[-2])
+
+    return fibonacci
 
 
-print(f"Voici les {n} premiers termes de la suite de Fibonacci : " + ', '.join(map(str, fibonacci)))
+print(f"Voici les {n} premiers termes de la suite de Fibonacci : " + ', '.join(map(str, fibonacci(n))))
