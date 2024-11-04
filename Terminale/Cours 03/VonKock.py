45f6bb308b60605bfffdcae6799c6c786113e80a
beltium <mathieuhugo2008@gmail.com>
2024-11-04 10:42:00 +0100

    Cours 03 Tle

--- Terminale/Cours 03/VonKock.py
+++ Terminale/Cours 03/VonKock.py
@@ -13,20 +13,16 @@ def von_koch(longueur, niveau):
         turtle.left(60)
         von_koch(longueur, niveau - 1)
 
-# Fonction pour dessiner le flocon de Von Koch complet
 def flocon_von_koch(longueur, niveau):
     for _ in range(3):
         von_koch(longueur, niveau)
         turtle.right(120)
 
-# Initialisation de Turtle
 turtle.speed("fastest")
 turtle.penup()
-turtle.goto(-200, 100)  # Position initiale pour centrer le flocon
+turtle.goto(-200, 100)
 turtle.pendown()
 
-# Appel de la fonction pour dessiner le flocon de Von Koch
-flocon_von_koch(400, 4)  # Longueur de 400 et 4 niveaux de récursivité
+flocon_von_koch(400, 4)
 
-# Finalisation du dessin
 turtle.done()
