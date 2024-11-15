fd4224bbc2e468fe3bf7caacf799700c23041f70
beltium <mathieuhugo2008@gmail.com>
2024-11-15 08:46:50 +0100

    Titre + centrage + taille fenêtre

--- Extra Exercises/Tkinter/Hello World/main2.py
+++ Extra Exercises/Tkinter/Hello World/main2.py
@@ -4,9 +4,11 @@ class Root(tk.Tk):
     def __init__(self):
         super().__init__()
 
-        self.label = tk.Label(self, text="Hello World", padx=5, pady=5)
+        self.label = tk.Label(self, text="Hello World", padx=5, pady=5, anchor="center")
+        self.label.pack(expand=True)
 
-        self.label.pack()
+        self.geometry("600x400")
+        self.title('Hello World')
 
 if __name__ == "__main__":
     root = Root()
