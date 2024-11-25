71e1c126e967b68b34d6babec7a7ad9e7f65d72f
beltium <mathieuhugo2008@gmail.com>
2024-11-25 10:29:43 +0100

    Cours 21 : Ajout d'un inventaire + variable boolean

--- Première/Trimestre 1/Cours 21/Personnage.py
+++ Première/Trimestre 1/Cours 21/Personnage.py
@@ -6,6 +6,8 @@ personnage["nom"] = input("Entrez le nom du personnage : ")
 personnage["age"] = int(input("Entrez l'âge du personnage : "))
 personnage["vie"] = int(input("Entrez les points de vie du personnage : "))
 personnage["niveau"] = int(input("Entrez le niveau du personnage : "))
+personnage["inventaire"] = input("Entrez les items de son inventaire (séparés par une virgule) : ").split(", ")
+personnage["réel"] = True if input("Le personnage est-il réelle ? (y/n) ").lower() == "y" else False
 
 # Afficher les informations du personnage
 print("\nInformations du personnage :")
@@ -13,6 +15,8 @@ print(f"Nom : {personnage['nom']}")
 print(f"Âge : {personnage['age']}")
 print(f"Points de vie : {personnage['vie']}")
 print(f"Niveau : {personnage['niveau']}")
+print(f"Inventaire : {", ".join(personnage["inventaire"])}")
+print("Le personnage est", "réel." if personnage["réel"] else "fictif.")
 
 # Modifier les points de vie et le niveau
 personnage["vie"] = int(input("\nEntrez les nouveaux points de vie : "))
