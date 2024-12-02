198193b22ed17c625d6f3b38146e16709c41768d
beltium <mathieuhugo2008@gmail.com>
2024-12-02 10:31:54 +0100

    Cours 23 : Ajout de commentaires

--- Première/Trimestre 1/Cours 23/Statistiques.py
+++ Première/Trimestre 1/Cours 23/Statistiques.py
@@ -1,20 +1,22 @@
 
-import statistics as stat
+"""Programme permettant de calculer des statistiques sur une liste"""
 
+import statistics as stat # Import de la lib
 
+# Focntion principale de calcule
 def calcul_statistiques(data):
     etendue = max(data) - min(data)
     moyenne = stat.mean(data)
-    median = stat.median(data)
-    ecart_type = stat.stdev(data) if len(data) > 1 else 0
-    return (etendue, moyenne, median, ecart_type)
+    median = stat.median(data) # Utilisation de la lib pour faire les calcules
+    ecart_type = stat.stdev(data) if len(data) > 1 else 0 # Un écart type d'une série de 1 valeur vaut 0
+    return (etendue, moyenne, median, ecart_type) # Return les valeurs dans un tuples
 
-données = input("Entrez vos valeurs (séparées par des virgules) : ").split(", ")
-données = [float(val) for val in données]
+données = input("Entrez vos valeurs (séparées par des virgules) : ").split(", ") # Séparation du string dans une liste
+données = [float(val) for val in données] # Convertissage des string en float
 
 etendue, moyenne, median, ecart_type = calcul_statistiques(données)
 
-print(f"\nL'étendue est de {etendue:.2f}.")
+print(f"\nL'étendue est de {etendue:.2f}.") # Print avec 2 chiffres après la virgule
 print(f"La moyenne est de {moyenne:.2f}.")
 print(f"La médianne est de {median:.2f}.")
 print(f"L'écart type est de {ecart_type:.2f}.")
