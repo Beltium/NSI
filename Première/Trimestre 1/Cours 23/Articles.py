3ff3ef042e01f3385304e2baf45acb33c0696687
beltium <mathieuhugo2008@gmail.com>
2024-12-03 08:25:09 +0100

    Cours 23 : Articles.py

---
+++ Première/Trimestre 1/Cours 23/Articles.py
@@ -0,0 +1,33 @@
+
+articles = []
+
+# Fonction print
+def print_art(articles):
+    if not articles:
+        print("Il n'y a pas encore d'articles.")
+    else:
+        for art in articles:
+            print(f"Titre: {art['titre']}, Auteur: {art['auteur']}, Date: {art['date']}\n")
+
+
+# Fonction d'ajout d'articles
+def add_art(titre, auteur, date, articles):
+    article = {"titre": titre, "auteur": auteur, "date": date}
+    articles.append(article)
+
+# Fonction principale
+def main():
+    global articles
+
+    titre = "Élections américaines 2024"
+    auteur = ("Jean Dupont")
+    date = "03/12/2024"
+
+    add_art(titre, auteur, date, articles)
+
+    print_art(articles)
+
+
+
+if __name__ == '__main__':
+    main()
