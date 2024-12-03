636bc0b6340aa21bee0d6e79bff0cc84cd9bdf8b
beltium <mathieuhugo2008@gmail.com>
2024-12-03 08:29:48 +0100

    Cours 23 : Possibilité de rajouter un article

--- Première/Trimestre 1/Cours 23/Articles.py
+++ Première/Trimestre 1/Cours 23/Articles.py
@@ -3,11 +3,12 @@ articles = []
 
 # Fonction print
 def print_art(articles):
+    print()
     if not articles: # Vérifier si il y a des articles
         print("Il n'y a pas encore d'articles.")
     else:
         for art in articles: # Les print un par un
-            print(f"Titre: {art['titre']}, Auteur: {art['auteur']}, Date: {art['date']}\n")
+            print(f"Titre: {art['titre']}, Auteur: {art['auteur']}, Date: {art['date']}")
 
 
 # Fonction d'ajout d'articles
@@ -26,7 +27,11 @@ def main():
     add_art(titre, auteur, date, articles)
     print_art(articles)
 
-
+    titre = input("Donner un titre à l'article : ")
+    auteur = input("Entrer le nom de l'auteur : ")
+    date = input("Entrer la date de publication : ")
+    add_art(titre, auteur, date, articles)
+    print_art(articles)
 
 if __name__ == '__main__':
     main()
