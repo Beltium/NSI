c0130cb5b3d977af19c80ae255f22f983cd9ac3c
beltium <mathieuhugo2008@gmail.com>
2024-12-10 08:41:47 +0100

    Cours 25 : read.py Ajout de commentaires

--- Première/Trimestre 1/Cours 25/read.py
+++ Première/Trimestre 1/Cours 25/read.py
@@ -1,12 +1,17 @@
 
+# Récupérer le file_path depuis write.py
 from write import file_path
 
+# Fonction de lecture
 def read(file_path=file_path):
     try:
         with open(file_path, 'r') as f:
+            # On retourne directement ce qui est lu
             return f.read()
     except Exception as e:
+        # Gestion des erreurs identiques à write.py
         print(f"Erreur : {e}")
 
+# Petit test
 contenu = read()
 print(contenu)
