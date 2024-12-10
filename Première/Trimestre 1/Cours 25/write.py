06dfc8995ca129c2cc1e859b296f5721e9b44313
beltium <mathieuhugo2008@gmail.com>
2024-12-10 08:40:31 +0100

    Cours 25 : write.py Ajout de commentaires

--- Première/Trimestre 1/Cours 25/write.py
+++ Première/Trimestre 1/Cours 25/write.py
@@ -1,15 +1,19 @@
 
+# Définir le chemin du fichier
 file_path = "file.txt"
 
+# Fonction d'écriture
 def write(str, file_path=file_path):
     try:
-        with open(file_path, 'w') as f:
+        with open(file_path, 'w') as f: # Équivalent de f = open()
             f.write(str)
             f.close()
     except Exception as e:
+        # Gestion des erreurs : afficher simplement l'erreur
         print(f"Erreur : {e}")
 
 write("Hello World!\n"
       "Hello World 2!\n"
       "It's work !")
 # write(2)
+# Test de gestion des erreurs : un int ne peut pas être écrit sans être transformé en str avant
