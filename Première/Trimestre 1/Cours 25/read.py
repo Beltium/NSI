dd0d16f4d64ee1019e679e3a6f44126bb941d537
beltium <mathieuhugo2008@gmail.com>
2024-12-10 08:37:57 +0100

    Cours 25 : read.py Ajout de gestion d'erreurs

--- Première/Trimestre 1/Cours 25/read.py
+++ Première/Trimestre 1/Cours 25/read.py
@@ -1,8 +1,12 @@
 
 from write import file_path
 
-f = open(file_path, 'r')
+def read(file_path=file_path):
+    try:
+        with open(file_path, 'r') as f:
+            return f.read()
+    except Exception as e:
+        print(f"Erreur : {e}")
 
-contenu = f.read()
-f.close()
+contenu = read()
 print(contenu)
