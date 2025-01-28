48450b0e035ec7ff071403e01c7af363b775017a
beltium <mathieuhugo2008@gmail.com>
2025-01-28 08:27:10 +0100

    Programme simple permettant de récup des données d'un sql depuis un prompt

---
+++ Première/SQL/Cours 28/Actus/load_data.py
@@ -0,0 +1,15 @@
+import sqlite3
+
+db = "actus.db"
+
+def load_data(prompt, db = db):
+    with sqlite3.connect(db) as connexion:
+        curseur = connexion.cursor()
+        curseur.execute(prompt)
+        return curseur.fetchall()
+
+prompt = '''
+SELECT * FROM actualites
+'''
+
+print(load_data(prompt))
