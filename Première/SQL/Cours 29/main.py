908ac6cace5e755b65b840090019ab5cb2d6674d
beltium <mathieuhugo2008@gmail.com>
2025-01-30 08:48:43 +0100

    Cours 29 : main()

--- Première/SQL/Cours 29/main.py
+++ Première/SQL/Cours 29/main.py
@@ -10,6 +10,7 @@ def add_player(name, score, path=db_path):
         VALUES ('{name}', {score})
         ''')
         connexion.commit()
+        print(f"{name} ajouté avec succès avec un score de {score}.")
 
 def input_player():
     while True:
@@ -32,7 +33,26 @@ def main():
         CREATE TABLE IF NOT EXISTS players (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             name TEXT NOT NULL,
-            score INTEGER NOT NULL,
-        )
-        ''')
+            score INTEGER NOT NULL
+        )''')
 
+        while True:
+            print("==== MENU ====")
+            print("1. AJouter un score.")
+            print("2. Afficher les 3 meilleurs scores.")
+            print("0. Quitter.")
+            choice = input("Entrer votre choix (1/2/0) : ")
+            match choice:
+                case '1':
+                    input_player()
+                    continue
+                case '2':
+                    print("a")
+                    continue
+                case '0':
+                    exit()
+                case _:
+                    print("Veuiller choisir un score ")
+
+if __name__ == '__main__':
+    main()
