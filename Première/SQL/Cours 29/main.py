8f447f3967390e62f920225ed223cf04be546e49
beltium <mathieuhugo2008@gmail.com>
2025-01-30 08:33:15 +0100

    Cours 29 : input player

--- Première/SQL/Cours 29/main.py
+++ Première/SQL/Cours 29/main.py
@@ -2,16 +2,6 @@ import sqlite3
 
 db_path = "players.db"
 
-with sqlite3.connect(db_path) as connexion:
-    curseur = connexion.cursor()
-    curseur.execute('''
-    CREATE TABLE IF NOT EXISTS players (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        name TEXT NOT NULL,
-        score INTEGER NOT NULL,
-    )
-    ''')
-
 def add_player(name, score, path=db_path):
     with sqlite3.connect(db_path) as connexion:
         curseur = connexion.cursor()
@@ -20,4 +10,29 @@ def add_player(name, score, path=db_path):
         VALUES ('{name}', {score})
         ''')
         connexion.commit()
+
+def input_player():
+    while True:
+        name = input("Entrer le nom du joueur ('fin' pour terminer) : ")
+        if name == 'fin':
+            break
+        while True:
+            try :
+                score = int(input("Entrer son score : "))
+            except ValueError:
+                print("Veuillez rentrer un nombre entier.")
+                continue
+            break
+        add_player(name, score)
+
+def main():
+    with sqlite3.connect(db_path) as connexion:
+        curseur = connexion.cursor()
+        curseur.execute('''
+        CREATE TABLE IF NOT EXISTS players (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL,
+            score INTEGER NOT NULL,
+        )
+        ''')
 
