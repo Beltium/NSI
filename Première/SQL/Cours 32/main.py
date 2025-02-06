b5aad984e7a7eee0cf3905ae1997b32a7c83c3b6
beltium <mathieuhugo2008@gmail.com>
2025-02-06 08:35:39 +0100

    Cours 32 : alice

--- Première/SQL/Cours 32/main.py
+++ Première/SQL/Cours 32/main.py
@@ -13,3 +13,15 @@ with sqlite3.connect(db_path) as connexion:
             );
     ''')
     connexion.commit()
+
+def add():
+    with sqlite3.connect(db_path) as connexion:
+        curseur = connexion.cursor()
+        curseur.execute('''
+        INSERT INTO contacts (nom, telephone, email)
+        VALUES ('Alice', '0645789716', 'aaaalice@gmail.com')
+        ''')
+        connexion.commit()
+
+if __name__ == '__main__':
+    add()
