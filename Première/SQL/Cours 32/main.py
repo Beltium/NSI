933bff67f63521a2fc5274371fdb7f06596f96fe
beltium <mathieuhugo2008@gmail.com>
2025-02-06 08:32:56 +0100

    Cours 32 : Create table

---
+++ Première/SQL/Cours 32/main.py
@@ -0,0 +1,15 @@
+import sqlite3
+
+db_path = 'contacts.db'
+
+with sqlite3.connect(db_path) as connexion:
+    curseur = connexion.cursor()
+    curseur.execute('''
+    CREATE TABLE IF NOT EXISTS contacts (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                nom VARCHAR(100),
+                telephone VARCHAR(15),
+                email VARCHAR(100)
+            );
+    ''')
+
